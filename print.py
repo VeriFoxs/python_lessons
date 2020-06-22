2354e121dea2921dc4b43e3e31d6d6c2dfca6d29
lyubomyr83 <lyubomyr83@mail.ru>
2020-06-22 14:56:27 +0300

    XD

--- print.py
+++ print.py
@@ -1,4 +1,4 @@
-# name = 'Денис'
+name = 'Даня'
 # age = 5
 # print('Меня зовут ',name,' и мне ', age,' лет.')
 
@@ -9,7 +9,7 @@
 # print(answer)
 
 # Вычислим сумму двух чисел
-number1 = input('введите первое число\n')
-number2 = input('введите второе число\n')
+number1 = input(f'{name}, введите первое число\n')
+number2 = input(f'{name}, введите второе число\n')
 answer = int(number1) + int(number2)
 print('Сумма числа ', number1, ' и числа ', number2, ' равна ', answer)
