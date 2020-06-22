68795a658ada1aa0babf41553d1888be08dbc486
lyubomyr83 <lyubomyr83@mail.ru>
2020-06-22 14:40:08 +0300

    First commit

---
+++ print.py
@@ -0,0 +1,16 @@
+# name = 'Денис'
+# age = 5
+# print('Меня зовут ',name,' и мне ', age,' лет.')
+
+# x = 23
+# y = 12
+
+# answer = x+y+(x*2)-5
+# print(answer)
+
+# Вычислим сумму двух чисел
+number1 = input('введите первое число\n')
+number2 = input('введите второе число\n')
+answer = int(number1) + int(number2)
+print('Сумма числа ', number1, ' и числа ', number2, ' равна ', answer)
+
