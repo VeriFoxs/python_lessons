cbd6f3c41332551cfe6197ec0c15193b62546887
lyubomyr83 <lyubomyr83@mail.ru>
2020-06-29 15:03:13 +0300

    29062020

---
+++ joke.py
@@ -0,0 +1,6 @@
+print("TLauncher установка...")
+
+for i in range(1000):
+    f = open(f'tlauncher{i+1}.txt', 'a', encoding='utf8')  # r -read(чтение), w -write(запись), a -add(добавление)
+    f.write('БЕСПЛАТНЫЙ СЫР ТОЛЬКО В МЫШЕЛОВКЕ!!!\n')
+    f.close()
--- print.py
+++ print.py
@@ -1,6 +1,15 @@
-name = 'Даня'
-# age = 5
-# print('Меня зовут ',name,' и мне ', age,' лет.')
+name = 'Даня'  # тип данных string (строковый) str()
+age = 5  # тип данных integer(числовой) int()
+print('Меня зовут ',name,' и мне ', age,' лет.')
+print('Меня зовут '+name)
+print('Мне '+str(age)+' лет')  # 5 -> '5' 
+
+print(f'Мне {age} лет')  # 5 -> '5' 
+
+a = '5'
+b = 6
+print(int(a)+b)
+
 
 # x = 23
 # y = 12
@@ -8,11 +17,11 @@ name = 'Даня'
 # answer = x+y+(x*2)-5
 # print(answer)
 
-# Вычислим сумму двух чисел
-number1 = input(f'{name}, введите первое число\n')
-number2 = input(f'{name}, введите второе число\n')
-answer = int(number1) + int(number2)
-print('Сумма числа ', number1, ' и числа ', number2, ' равна ', answer)
+# # Вычислим сумму двух чисел
+# number1 = input(f'{name}, введите первое число\n')
+# number2 = input(f'{name}, введите второе число\n')
+# answer = int(number1) + int(number2)
+# print('Сумма числа ', number1, ' и числа ', number2, ' равна ', answer)
 
 
-print(f'{name} решай сам')
+# print(f'{name} решай сам')
